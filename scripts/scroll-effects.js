4c7f6060956912bb3cf1a93f49cfc500eea385ad
Lito Karageorgou <13-13995@saeinstitute.edu>
2025-03-05 14:07:09 +0200

    Finish styling and adding final text to about page

--- scripts/scroll-effects.js
+++ scripts/scroll-effects.js
@@ -25,3 +25,19 @@ const observerBoxes = new IntersectionObserver((entries, obs) => {
 document.querySelectorAll('.contact-info__box').forEach(box => {
   observerBoxes.observe(box);
 });
+
+// Observer for values
+const observerValues = new IntersectionObserver((entries, obs) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const h3 = entry.target.querySelector('h3');
+      if (h3) h3.classList.add('animate');
+      obs.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.5 });
+
+document.querySelectorAll('.values__value').forEach(box => {
+  observerValues.observe(box);
+});
+
