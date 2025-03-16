1749a1283ef7b577db95561ff7c1722133adb6fe
Lito Karageorgou <13-13995@saeinstitute.edu>
2025-03-16 02:38:14 +0200

    Start responsive design

--- scripts/loader.js
+++ scripts/loader.js
@@ -2,5 +2,5 @@ window.addEventListener('load', function () {
     setTimeout(function () {
       document.getElementById('loader').style.display = 'none';
       document.getElementById('main-container').style.display = 'block';
-    }, 2000); 
+    }, 500000); 
   });
