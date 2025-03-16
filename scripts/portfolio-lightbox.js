be61ad9f535e54fd89819cab03317bb02a6ccab3
Lito Karageorgou <13-13995@saeinstitute.edu>
2025-03-16 16:11:56 +0200

    Add modal to portfolio gallery

---
+++ scripts/portfolio-lightbox.js
@@ -0,0 +1,26 @@
+const modal = document.getElementById('imageModal');
+const modalImg = document.getElementById('expandedImg');
+const captionText = document.getElementById('caption');
+const closeBtn = document.querySelector('.close');
+const lightboxGallery = document.getElementById('portfolio__gallery');
+
+// Open modal and display image when user clicks on image
+lightboxGallery.addEventListener('click', (e) => {
+    if (e.target && e.target.classList.contains('portfolio__image')) {
+        modal.classList.add('show');
+        modalImg.src = e.target.src;
+        captionText.textContent = e.target.alt;
+    }
+});
+
+// Close modal when user clicks on close button
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('show'); 
+});
+
+// Close modal if user clicks outside the image
+modal.addEventListener('click', e => {
+    if (e.target === modal) {
+        modal.classList.remove('show');
+    }
+});
