944333032fcd042addffc15670cf78dbd15363a4
Lito Karageorgou <13-13995@saeinstitute.edu>
2025-03-16 02:00:58 +0200

    Edit navbar and index hero section styles

--- scripts/loader.js
+++ scripts/loader.js
@@ -2,5 +2,5 @@ window.addEventListener('load', function () {
     setTimeout(function () {
       document.getElementById('loader').style.display = 'none';
       document.getElementById('main-container').style.display = 'block';
-    }, 10); 
+    }, 2000); 
   });
