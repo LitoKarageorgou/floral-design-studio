92ca8a2f7a59f6751cd11001b5d13a88d09b67ec
Lito Karageorgou <13-13995@saeinstitute.edu>
2025-03-20 20:42:51 +0200

    Change name from 'modal' to 'lightbox' and fix BEM

--- scripts/portfolio-lightbox.js
+++ scripts/portfolio-lightbox.js
@@ -1,27 +1,27 @@
-const modal = document.getElementById('imageModal');
-const modalImg = document.getElementById('expandedImg');
+const lightbox = document.getElementById('imageLightbox');
+const lightboxImg = document.getElementById('expandedImg');
 const captionText = document.getElementById('caption');
-const closeBtn = document.querySelector('.close');
+const closeBtn = document.querySelector('.lightbox__close');
 const lightboxGallery = document.getElementById('portfolio__gallery');
 
-// Open modal and display image when user clicks on image
+// Open lightbox and display image when user clicks on image
 lightboxGallery.addEventListener('click', (e) => {
     if (e.target && e.target.classList.contains('portfolio__image')) {
-        modal.classList.add('show');
-        modalImg.src = e.target.src;
+        lightbox.classList.add('show');
+        lightboxImg.src = e.target.src;
         const description = e.target.parentElement.querySelector('p')?.textContent || '';
         captionText.textContent = description;
     }
 });
 
-// Close modal when user clicks on close button
+// Close lightbox when user clicks on close button
 closeBtn.addEventListener('click', () => {
-    modal.classList.remove('show');
+    lightbox.classList.remove('show');
 });
 
-// Close modal if user clicks outside the image
-modal.addEventListener('click', e => {
-    if (e.target === modal) {
-        modal.classList.remove('show');
+// Close lightbox if user clicks outside the image
+lightbox.addEventListener('click', e => {
+    if (e.target === lightbox) {
+        lightbox.classList.remove('show');
     }
 });
